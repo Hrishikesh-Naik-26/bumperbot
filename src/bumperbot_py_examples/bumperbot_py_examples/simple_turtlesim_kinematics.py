7468c4f5d32736be1b54086bee177f210633049f
Hrishikesh-Naik-26 <hrishikesh.n@somaiya.edu>
2026-09-13 20:19:07 +0530

    Updated simple turtlesim kinematics node with translation and rotation

--- src/bumperbot_py_examples/bumperbot_py_examples/simple_turtlesim_kinematics.py
+++ src/bumperbot_py_examples/bumperbot_py_examples/simple_turtlesim_kinematics.py
@@ -1,13 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
+import math
 
 class SimpleTurtlesimKinematics(Node):
     def __init__(self):
         super().__init__("simple_turtlesim_kinematics")
 
         self.turtle1_pose_sub_ = self.create_subscription(Pose, "/turtle1/pose", self.turtle1PoseCallback, 10)
-        self.turtle2_pose_sub_ = self.create_subscription(Pose, "/larp/pose", self.turtle2PoseCallback, 10)
+        self.turtle2_pose_sub_ = self.create_subscription(Pose, "/turtle2/pose", self.turtle2PoseCallback, 10)
 
         self.last_turtle1_pose_ = Pose()
         self.last_turtle2_pose_ = Pose()
@@ -21,10 +22,18 @@ class SimpleTurtlesimKinematics(Node):
         Tx = self.last_turtle2_pose_.x - self.last_turtle1_pose_.x
         Ty = self.last_turtle2_pose_.y - self.last_turtle1_pose_.y
 
+        theta_rad = self.last_turtle2_pose_.theta - self.last_turtle1_pose_.theta
+        theta_deg = 180*theta_rad/3.14
+
         self.get_logger().info(f"""\n\
             Translation Vector turtle1 -> larp \n
             Tx: {Tx}
-            Ty: {Ty}""")
+            Ty: {Ty}
+            Rotation Matrix Turtle1 -> Turtle2
+            theta(rad): {theta_rad}
+            theta(deg): {theta_deg}
+            |R11   R12| : {math.cos(theta_rad)}  {-math.sin(theta_rad)}
+            |R21   R22| : {math.sin(theta_rad)}  {math.cos(theta_rad)}""")
 
 def main():
     rclpy.init()
